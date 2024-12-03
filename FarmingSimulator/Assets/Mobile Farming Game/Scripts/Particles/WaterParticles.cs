4a0bb71ebe7932176ee32c5e1163d2e38baae0ef
umitcanyucesoy <umitcanyucesoy@gmail.com>
2024-12-03 04:47:37 +0300

    WaterParticle, PlayerToolSelector script

---
+++ FarmingSimulator/Assets/Mobile Farming Game/Scripts/Particles/WaterParticles.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Mobile_Farming_Game.Scripts.Particles
+{
+    public class WaterParticles : MonoBehaviour
+    {
+        
+    }
+}
---
+++ FarmingSimulator/Assets/Mobile Farming Game/Scripts/Player/PlayerToolSelector.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Mobile_Farming_Game.Scripts.Player
+{
+    public class PlayerToolSelector : MonoBehaviour
+    {
+        
+    }
+}
